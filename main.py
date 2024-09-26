66a3233b4971d441b52eb88104af0e2a0d194c06
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2024-09-26 16:49:44 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# All Rights Reserved
+# Authors:
+# - Lucas Placentino
+# - Chloé Blommaert
+# - Numa Deville
+
+def main():
+    pass
+
+if __name__ = "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(e)
+    finally:
+        print("###End program###")
