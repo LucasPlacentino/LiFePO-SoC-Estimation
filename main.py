dba197588cb212844460c0bd0c3e43cac693e205
LucasPlacentino <23436953+LucasPlacentino@users.noreply.github.com>
2024-10-17 16:26:57 +0200

    read data and logs

--- main.py
+++ main.py
@@ -11,18 +11,34 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 import scipy as sp
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] :\n%(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+log = logging.getLogger(__name__)
 
 # function to read data from xlsx file:
-def read_excel_data(filename: str):
+def read_excel_data(filename: str, **kwargs) -> pd.DataFrame:
     # read data from xlsx file
-    data = pd.read_excel(filename)
+    data = pd.read_excel(filename, **kwargs)
     return data
 
 
 def main():
     # 1. Read data from OCV-SOC xlsx file
     # make lookup table from it - OR - create a fitting math func
-    ocv_soc_data = read_excel_data("data/Cha_Dis_OCV_SOC_Data.xlsx")
+    charging_ocv_soc_data: pd.DataFrame = read_excel_data("data/Cha_Dis_OCV_SOC_Data.xlsx", usecols="B:C", skiprows=1, names=["SOC", "OCV"])
+    log.debug(charging_ocv_soc_data)
+    discharging_ocv_soc_data: pd.DataFrame = read_excel_data("data/Cha_Dis_OCV_SOC_Data.xlsx", usecols="E:F", skiprows=1, names=["SOC", "OCV"])
+    log.debug(discharging_ocv_soc_data)
+    
+
+    #charging_ocv_soc_data.plot(x="SOC", y="OCV", kind="line", title="Charging OCV-SOC data")
+    #discharging_ocv_soc_data.plot(x="SOC", y="OCV", kind="line", title="Discharging OCV-SOC data")
+
 
 
     # -------------------------
