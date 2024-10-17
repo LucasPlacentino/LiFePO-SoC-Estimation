1708260ad8b1fa2506ee5283cfab64d5bba1a873
LucasPlacentino <23436953+LucasPlacentino@users.noreply.github.com>
2024-10-17 15:58:40 +0200

    add dependencies versions

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
-# All Rights Reserved
-# Authors:
-# - Lucas Placentino
-# - Chloé Blommaert
-# - Numa Deville
+"""
+All Rights Reserved
+
+Authors:
+  - Lucas Placentino
+  - Chloé Blommaert
+  - Numa Deville
+"""
 
 import pandas as pd
 import numpy as np
 import matplotlib as plt
 import scipy as sp
 
+# function to read data from xlsx file:
+def read_excel_data(filename: str):
+    # read data from xlsx file
+    data = pd.read_excel(filename)
+    return data
+
+
 def main():
+    # 1. Read data from OCV-SOC xlsx file
+    # make lookup table from it - OR - create a fitting math func
+    ocv_soc_data = read_excel_data("data/Cha_Dis_OCV_SOC_Data.xlsx")
+
+
+    # -------------------------
+    # 2. Model
+
+
+
+    # -------------------------
+    # 3. Optimization/Filtering
+    # Extended Kalman Filter
+    # Unscented Kalman Filter
+
+    # Error adjustment
+
+
     pass
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     try:
         main()
     except Exception as e:
